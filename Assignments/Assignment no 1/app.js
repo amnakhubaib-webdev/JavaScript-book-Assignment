04f800425ab835cd2f09d5e1deeec5173e16a501
Amna khubaib <amnakhubaib123@gmail.com>
2023-06-09 21:46:20 +0500

    task 1

---
+++ Assignments/Assignment no 1/app.js
@@ -0,0 +1,38 @@
+// Assignment 1 = Alert
+
+// Question no 1
+//Write a script to greet your website visitor using JS alert box
+
+// alert("Hello visitors ! ")
+
+// Question no 2
+// Write a script to display following message on your web page:
+
+// alert("Error! Plesase enter a valid password.")
+
+// Question no 3
+// Write a script to display following message on your web page: (Hint : Use line break)
+
+// alert("Welcome to JS Land... \n Happy Coding!");
+
+// Question no 4
+// Write a script to display following messages in sequence
+
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!")
+
+// Question no 5
+// Generate the following message through browser’s developer console:
+
+// console.log("Hello... I can run through my web browser's console")
+
+// Question no 6
+// Make use of alerts in your new/existing HTML & CSS project.
+
+// alert("Let's go")
+
+
+// Question no 7
+// Practice placement of <script></script> element in following sections of your project in exercise 6:a. Head b. Body (before your page’s HTML c. Body (inside your page’s HTML)d. Body (after your page’s HTML)
+
+//its done in html.
