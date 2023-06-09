182c2c75221bbc276d8ae40a79106633c522d019
Amna khubaib <amnakhubaib123@gmail.com>
2023-06-10 02:30:50 +0500

    completed

---
+++ Assignments/Assignment 26-30/app.js
@@ -0,0 +1,17 @@
+//Assigment 26-30 MATH METHODS 
+// Question 1
+Write a program that takes a positive integer from user &
+display the following in your browser.
+a. number
+b. round off value of the number
+c. floor value of the number
+d. ceil value of the number
+
+
+//Question 2
+Write a program that takes a negative floating point
+number from user & display the following in your browser.
+a. number
+b. round off value of the number
+c. floor value of the number
+d. ceil value of the number
